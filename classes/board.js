1a934ac3d30ec367e5edb9ccf5d3e85b7be26a9f
Joel Kram <joelnskram@gmail.com>
2024-11-28 16:28:53 +0000

    added a winstate and player move ish

--- classes/board.js
+++ classes/board.js
@@ -29,28 +29,62 @@ class Gameboard {
       return row.every((cell) => cell === " ") //looping through row/col to check if empty
       
     });
-    return isEmpty ? "Board is empty" : "Board has moves";
+    return isEmpty ? true : false
 
   }
     
+  //player turn - how will player object interact? or how to know who's turn it is?
+  playerTurn() {
+    const player_1 = 0;
+    const player_2 = 0;
+    
+  }
 
   //have I won or lost?
   gameEnd() {
-    //check cells for 3 in a row, take care of diagonals
-    
-    //this checks if board is full
-    if (this.isBoardEmpty === true) {
-      return "game over"
-    }
-  }
+    //which player is saved in variable here
+    const checkWin = (player) => {
+      //check the rows are equal to player input
+      const winRow = this.board.some((row) => {
+        row.every((cell) => cell == player)
+      });
+      //check cols for same
+      const winCol = [0, 1, 2].some((col) => {
+        this.board.every((row) => row[col] === player)
+      });
+
+      const inDiagonal = this.board[0][0] === player && this.board[1][1] === player && this.board[2][2] === player;
+      const ANTIinDiagonal = this.board[0][2] === player && this.board[1][1] === player && this.board[2][0] === player;
+
+      return winRow || winCol || inDiagonal || ANTIinDiagonal;
+    };
+
+    //check if player won
+    if (checkWin("X")) {
+      console.log("You Won");
+      return true;
+    } 
+
+    //check comp won
+    if (checkWin("0")) {
+      console.log("You Lost");
+      return false;
+    } 
+};
 };
 
-const board = new Gameboard();
-board.printBoard(); //prints board to screen
-for (let i = 0; i < 9; i ++) {
-  board.makeMove(0, 0, "X") //top left corner
+
+
+
+
+
+let board = new Gameboard();
+for (let i = 0; i < 3; i ++) { //quickly fill board with moves
+  for (let j = 0; j< 3; j++) {
+    board.makeMove(i, j, "X") //top left corner
+  }
+  
 }
-
-
-board.printBoard();
-console.log(board.isBoardEmpty()); //check if anything on board
+board.printBoard(); //prints board to screen
+// console.log(board.gameEnd()); //check if anything on board
+board.gameEnd();
